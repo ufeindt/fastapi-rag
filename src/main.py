7284b50a988454b682ba6904384aa94bb24d22cb
Ulrich Feindt <ufeindt@gmail.com>
2024-08-12 18:28:16 +0200

    Added a test htmx form

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
+from typing import Annotated, Union
+from uuid import uuid4
+
 from db import qdrant_client
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Form, Header, HTTPException, Request
+from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.gzip import GZipMiddleware
-from fastapi.responses import StreamingResponse
+from fastapi.responses import HTMLResponse, JSONResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from llm import OpenAiMessage, openai_query, openai_query_async
@@ -17,9 +21,71 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.add_middleware(GZipMiddleware)
 
 
-@app.get("/")
+class Todo:
+    def __init__(self, text: str):
+        self.id = uuid4()
+        self.text = text
+        self.done = False
+
+
+todos = []
+
+
+@app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("base.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="base.html")
+
+
+@app.get("/todos", response_class=HTMLResponse)
+async def list_todos(
+    request: Request, hx_request: Annotated[Union[str, None], Header()] = None
+):
+    if hx_request:
+        return templates.TemplateResponse(
+            request=request, name="todos.html", context={"todos": todos}
+        )
+    return JSONResponse(content=jsonable_encoder(todos))
+
+
+@app.post("/todos", response_class=HTMLResponse)
+async def create_todo(request: Request, todo: Annotated[str, Form()]):
+    todos.append(Todo(todo))
+    return templates.TemplateResponse(
+        request=request, name="todos.html", context={"todos": todos}
+    )
+
+
+@app.put("/todos/{todo_id}", response_class=HTMLResponse)
+async def update_todo(request: Request, todo_id: str, text: Annotated[str, Form()]):
+    for todo in todos:
+        if str(todo.id) == todo_id:
+            todo.text = text
+            break
+    return templates.TemplateResponse(
+        request=request, name="todos.html", context={"todos": todos}
+    )
+
+
+@app.post("/todos/{todo_id}/toggle", response_class=HTMLResponse)
+async def toggle_todo(request: Request, todo_id: str):
+    for todo in todos:
+        if str(todo.id) == todo_id:
+            todo.done = not todo.done
+            break
+    return templates.TemplateResponse(
+        request=request, name="todos.html", context={"todos": todos}
+    )
+
+
+@app.post("/todos/{todo_id}/delete", response_class=HTMLResponse)
+async def delete_todo(request: Request, todo_id: str):
+    for index, todo in enumerate(todos):
+        if str(todo.id) == todo_id:
+            del todos[index]
+            break
+    return templates.TemplateResponse(
+        request=request, name="todos.html", context={"todos": todos}
+    )
 
 
 class QueryForm(BaseModel):
